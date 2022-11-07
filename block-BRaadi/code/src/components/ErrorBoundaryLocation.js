bcf34e0c19ca0219260522d8f8f35fdf044b33ac
raj shringi <raj24shringi@gmail.com>
2022-11-07 13:46:20 +0530

    create different error boundary

---
+++ block-BRaadi/code/src/components/ErrorBoundaryLocation.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class ErrorBoundaryLocation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    console.log(error);
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong! Reload the page Location is empty</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundaryLocation;
